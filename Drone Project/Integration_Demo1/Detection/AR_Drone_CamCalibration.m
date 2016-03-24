83505cdbfcd3b6b06798cb5329e0adc1684a95ea
nestorhr <n.hernandez.rodriguez@tue.nl>
2016-03-24 11:16:32 +0100

    Updated comments on detection and refereeing files
    Deleted older files not used

--- Drone Project/Integration_Demo1/Detection/AR_Drone_CamCalibration.m
+++ Drone Project/Integration_Demo1/Detection/AR_Drone_CamCalibration.m
@@ -17,20 +17,3 @@ worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 [cameraParams, imagesUsed, estimationErrors] = estimateCameraParameters(imagePoints, worldPoints, ...
     'EstimateSkew', false, 'EstimateTangentialDistortion', false, ...
     'NumRadialDistortionCoefficients', 2, 'WorldUnits', 'mm');
-
-% View reprojection errors
-% h1=figure; showReprojectionErrors(cameraParams, 'BarGraph');
-
-% Visualize pattern locations
-% h2=figure; showExtrinsics(cameraParams, 'CameraCentric');
-
-% Display parameter estimation errors
-% displayErrors(estimationErrors, cameraParams);
-
-% For example, you can use the calibration data to remove effects of lens distortion.
-% originalImage = imread(imageFileNames{1});
-% undistortedImage = undistortImage(originalImage, cameraParams);
-
-% See additional examples of how to use the calibration data.  At the prompt type:
-% showdemo('MeasuringPlanarObjectsExample')
-% showdemo('SparseReconstructionExample')
