7a54f7d392fa152f96d18ada7a346e7215704d0b
Tom <t.zwijgers@gmail.com>
2016-04-15 16:14:59 +0200

    Clean up repository
    Renamed files and deleted unused items for clarification.

--- Drone Project/UWD Localization/xy2ij.m
+++
@@ -1,9 +0,0 @@
-function [i,j] = xy2ij(x,y,xm,ym,ni,nj)
-% Function to convert a symmetrical x,y axis coordinate system to a
-% row, column based matrix coordinate system
-% x, y      = coordinates
-% xm, ym    = max axis
-% ni, nj    = number of rows/colums
-    i = round(ni*(0.5 - y/ym));
-    j = round(nj*(0.5 + x/xm));
-    
